9ca767506c6069b98b08237cdf018135269cc9ab
Min Kyung CHOI <haleychoi0918@gmail.com>
2020-08-25 11:20:21 +1000

    create responsive covid-19 tracker app

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import { Cards, Chart, CountryPicker } from './components'
 import styles from './App.module.css'
 import { fetchData } from './api'
 
+import covidTrackerImage from './images/covid_tracker.png'
+
 class App extends React.Component {
     state = {
         data: {}, 
+        country: ''
     }
 
     async componentDidMount(){
@@ -15,14 +18,22 @@ class App extends React.Component {
         this.setState({ data: fetchedData })
     }
 
+    handleCountryChange = async(country)=>{
+        // fetch the data
+        const fetchedData = await fetchData(country)
+        this.setState({ data: fetchedData, country: country })
+        // set the state
+    }
+
     render(){
-        const { data } = this.state
-        
+        const { data, country } = this.state
+
         return(
             <div className={styles.container}>
+                <img className={styles.image} src={covidTrackerImage} alt="covid-19"/>
                 <Cards data={data}/>
-                <CountryPicker />
-                <Chart />
+                <CountryPicker handleCountryChange={this.handleCountryChange}/>
+                <Chart data={data} country={country}/>
             </div>
         )
     }
