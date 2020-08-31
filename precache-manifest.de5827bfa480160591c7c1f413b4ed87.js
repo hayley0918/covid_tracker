0d0aadbdb3cd42035834ba55edb95d695c9fd3d3
Min Kyung CHOI <haleychoi0918@gmail.com>
2020-08-31 10:34:12 +1000

    Updates

---
+++ precache-manifest.de5827bfa480160591c7c1f413b4ed87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0739a735631e50674f07a56a74b2b4d0",
+    "url": "/covid_tracker/index.html"
+  },
+  {
+    "revision": "fd69f27d9cba7f632700",
+    "url": "/covid_tracker/static/css/main.545081d5.chunk.css"
+  },
+  {
+    "revision": "7d6adc6ae5790fa2aa66",
+    "url": "/covid_tracker/static/js/2.4facd355.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covid_tracker/static/js/2.4facd355.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd69f27d9cba7f632700",
+    "url": "/covid_tracker/static/js/main.e2f30cd9.chunk.js"
+  },
+  {
+    "revision": "4c5145b2abee34e7044b",
+    "url": "/covid_tracker/static/js/runtime-main.fb184295.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid_tracker/static/media/covid_tracker.d7265326.png"
+  }
+]);
